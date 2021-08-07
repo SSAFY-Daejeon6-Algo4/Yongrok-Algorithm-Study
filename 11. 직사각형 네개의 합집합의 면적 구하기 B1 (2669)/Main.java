8beb0d113e09cc7d8e8b97fd95ce70b58d298d82
오용록 <ohyr9685@gmail.com>
2021-08-08 00:01:07 +0900

    0808_STUDY

--- 11. 직사각형 네개의 합집합의 면적 구하기 B1 (2669)/Main.java
+++ 11. 직사각형 네개의 합집합의 면적 구하기 B1 (2669)/Main.java
@@ -1,5 +1,3 @@
-package com.ohyr.boj.problem2669;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
--- 18. 색종이 B1 (10163)/Main.java
+++ 18. 색종이 B1 (10163)/Main.java
@@ -1,5 +1,3 @@
-package com.ohyr.boj.problem10163;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
