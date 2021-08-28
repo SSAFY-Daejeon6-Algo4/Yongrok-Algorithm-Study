64d361303f10a35cee4ce1570091f438c75437b1
ohyr <ohyr9685@gmail.com>
2021-08-28 13:22:09 +0900

    0829_STUDY

---
+++ 알고리즘 문제 2/14. 카드2 S4 (2164)/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(in.readLine());
+		
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i=1;i<=n;i++) {
+			q.offer(i);
+		}
+		
+		int cnt = 0;
+		
+		while(q.size() > 1){
+			int tmp = q.poll();
+			
+			if(cnt%2 == 1) {
+				q.offer(tmp);
+			}
+
+			cnt++;
+		}
+		
+		System.out.println(q.poll());
+	}
+
+}
